7471d7618d264394b1434664a757f4dbc207cebc
gerardet46 <gerrysoft@outlook.es>
2021-08-13 13:08:33 +0200

    zsh now works!

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -4,9 +4,9 @@ pac_ins zsh
 
 # copy config
 mkdir -p "$CONFIG_DIR/zsh"
-cp pkgs/zsh/zshrc "$CONFIG_DIR/zsh/zshrc"
+sed "s/AUR\\_HELPER.*$/AUR\\_HELPER\\=\\\"$AUR_HELPER\\\"" pkgs/zsh/zshrc > "$CONFIG_DIR/zsh/zshrc"
 ln -f "$CONFIG_DIR/zsh/zshrc" "$HOME/.zshrc"
 
 # make it default
 echo "Enter password to change the default shell to ZSH"
-chsh /bin/zsh
+chsh -s /bin/zsh
