a737998e85c8acd1b216cc6a100adcf6be3dee16
gerardet46 <gerrysoft@outlook.es>
2021-08-21 10:39:44 +0200

    remove (BE) indications in pkgs_selection.sh

--- pkgs_selection.sh
+++ pkgs_selection.sh
@@ -4,7 +4,6 @@
 # SELECT PROGRAMS HERE
 
 # uncomment script name to be installed
-# comments starting with "BE" means that are included in `base-ext`
 # comments starting with "SR" means that are compiled from source
 
 ####################################
@@ -18,12 +17,12 @@ ger_ins "base-min"        # aur, arch mirrors, pacman-contrib, fakeroot, dash, b
 
 # UTILITIES
 #ger_ins "zsh"             # the best POSIX-compliant shell
-#ger_ins "st"              # (BE, SR) the best terminal
-#ger_ins "dmenu"           # (BE, SR) the menu
-#ger_ins "scripts"         # (BE) scripts (installs dmenu if not installed)
+#ger_ins "st"              # (SR) the best terminal
+#ger_ins "dmenu"           # (SR) the menu
+#ger_ins "scripts"         # scripts (installs dmenu if not installed)
 #ger_ins "dunst"           # for notifications
-#ger_ins "slock"           # (BE, SR) the locker
-#ger_ins "tabbed"          # needed for nnn preview
+#ger_ins "slock"           # (SR) the locker
+#ger_ins "tabbed"          # (SR) needed for nnn preview
 #ger_ins "nnn"             # (SR) file manager (includes tabbed)
 #ger_ins "emacs"           # the best OS
 #ger_ins "nvim"            # the best text editor
