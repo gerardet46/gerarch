8404dde4e0c8e3797d37f55dbbf2930217eb7ef7
gerardet46 <gerrysoft@outlook.es>
2021-12-12 17:45:37 +0100

    add gcolors env to autostart

--- pkgs/gcolors/install.sh
+++ pkgs/gcolors/install.sh
@@ -4,4 +4,6 @@ if ! [ -f /usr/include/gcolors.h ]; then
     git clone "$GER_GITHUB/gcolors.git" "build/gcolors"
     cd build/gcolors
     sudo ./install.sh
+
+    add_autostart ". /opt/gcolors/colors_env.sh"
 fi
